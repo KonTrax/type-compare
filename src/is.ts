4cf7e18665dfc5f715a3ee52874beba9d4435489
BeA Haehre <kontraxbea+git@gmail.com>
2018-12-17 23:37:36 +0100

    removed unnecessary steps

--- src/is.ts
+++ src/is.ts
@@ -5,18 +5,16 @@ export type IsValue <T, THEN = T, ELSE = NEV> =
 		IsSpecial<T, ELSE, THEN>
 
 export type IsSpecial <T, THEN, ELSE = NEV> =
-		[UNK] extends [T]   ? THEN : // unknown | any
 		[T]   extends [NEV] ? THEN : // never
+		[UNK] extends [T]   ? THEN : // unknown | any
 		ELSE
 
 export type IsNever <T, THEN, ELSE = NEV> =
-		[UNK] extends [T]   ? ELSE : // unknown | any
 		[T]   extends [NEV] ? THEN : // never
 		ELSE
 
 export type IsUnknownOrAny <T, THEN, ELSE = NEV> =
 		[UNK] extends [T]   ? THEN : // unknown | any
-		[T]   extends [NEV] ? ELSE : // never
 		ELSE
 
 export type IsUnknown <T, THEN, ELSE = NEV> =
