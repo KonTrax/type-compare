cb2dfa5d2a21861361075f055ede70ccaaa8ede6
BeA Haehre <kontraxbea+git@gmail.com>
2018-12-18 00:57:39 +0100

    Better Equals & tests

--- src/Equals.ts
+++ src/Equals.ts
@@ -1,4 +1,64 @@
-export type Equals <T1, T2, THEN = T1, ELSE = never> =
-		[T1 & T2] extends [never]   ? ELSE :
-		[unknown] extends [T1 | T2] ? ELSE :
-		[T1, T2]  extends [T2, T1]  ? THEN : ELSE
+import {
+	IsNever,
+} from './is'
+
+type NEV = never
+type UNK = unknown
+
+/**
+ * Strict type equality comparison
+ */
+export type Equals <A, B, THEN = A, ELSE = never> =
+(
+		| // (A || B) === NEV
+		[A & B] extends [NEV]   ? IsNever<A | B, THEN, ELSE>
+
+		: // (A || B) === UNK | ANY
+		[UNK]   extends [A | B] ? UnknownOrAny<A, B, THEN, ELSE>
+
+		: // (A == B) === VAL
+		[A, B]  extends [B, A]  ? THEN
+
+		: // (A != B) === ?
+		ELSE
+)
+
+/**
+ * Strict type equality comparison (value types only)
+ */
+export type EqualValues <A, B, THEN = A, ELSE = never> =
+		[A & B] extends [never] ? ELSE : // (A || B) === NEV
+		[UNK]   extends [A | B] ? ELSE : // (A || B) === UNK | ANY
+		[A, B]  extends [B, A]  ? THEN : // (A == B) === VAL
+		/*                     */ ELSE   // (A != B) === ?
+
+/**
+ * Equality comparison of `unknown` and `any`
+ */
+export type UnknownOrAny <A, B, THEN, ELSE = never> =
+		[UNK, UNK] extends [A, B]
+				? Keys<A, B, THEN, ELSE> // (A || B) === UNK | ANY
+				: ELSE                   // (A || B) === ?
+
+/* NOTES
+  *
+  * Keyof special types
+  * - keyof any       === string | number | symbol
+  * - keyof unknown   === never
+  * - keyof never     === never
+  * - keyof void      === never
+  *
+  * Not all value types has keys
+  * - keyof object    === never
+  * - keyof {}        === never
+  * - keyof null      === never
+  * - keyof undefined === never
+  */
+
+/**
+ * // REVIEW
+ */
+type Keys <A, B, THEN, ELSE = never> =
+		[keyof A, keyof B] extends [keyof B, keyof A]
+				? THEN // (keyof A == keyof B) === ?
+				: ELSE // (keyof A != keyof B) === ?
