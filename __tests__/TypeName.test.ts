abe31e33393a80b0d2026321be9819ea3f6a95ba
BeA Haehre <kontraxbea+git@gmail.com>
2018-12-20 08:14:03 +0100

    better structure

--- __tests__/TypeName.test.ts
+++ __tests__/TypeName.test.ts
@@ -1,51 +1,100 @@
 import './helpers'
-import * as _ from '@ktb/type-compare'
+import {Equals} from '@ktb/type-compare'
+import {TypeName as Target} from '@ktb/type-compare'
 
-namespace TypeName { import Target = _.TypeName
+//==============================================================================
+//=== Setup ===
 
-	type Test <IN, EXP> = _.Equals<IN, EXP, TRU, FAL>
+type  Test   <A, B> = Equals<A, B, TRU, FAL>
 
-	function Test <EXP, IN> ()                         :Test<IN, EXP>
-	function Test <EXP, IN> (opts :{EXP :EXP, IN :IN}) :Test<IN, EXP>
-	function Test <EXP, IN> (       EXP :EXP, IN :IN)  :Test<IN, EXP>
-	function Test           () :any {}
+const Test = (() => ANY) as {
+		<EXP>                       () :Test<EXP, Target>
+		<EXP, IN, RES = Target<IN>> () :Test<EXP, RES>
+		<EXP, IN, RES = Target<IN>> () :Test<EXP, RES>
 
-	//####################
-
-	;{type IN  = never
-		type EXP = never
-		;    TRU = Test<EXP, Target<IN>>()}
-
-	;{type IN  = unknown
-		type EXP = 'object'
-		;    TRU = Test<EXP, Target<IN>>()}
-
-	;{type IN  = any
-		type EXP =
-				| 'string' | 'number' | 'boolean' | 'symbol'
-				| 'undefined'
-				| 'function' | 'object'
-		;    TRU = Test<EXP, Target<IN>>()}
-
-	//####################
-
-	;{type IN  = void
-		type EXP = 'object'
-		;    TRU = Test<EXP, Target<IN>>()}
-
-	//####################
-
-	;{type IN  = Object
-		type EXP = 'object'
-		;    TRU = Test<EXP, Target<IN>>()}
-
-	;{type IN  = object
-		type EXP = 'object'
-		;    TRU = Test<EXP, Target<IN>>()}
-
-	;{type IN  = {}
-		type EXP = 'object'
-		;    TRU = Test<EXP, Target<IN>>()}
-
-	//####################
+		<EXP extends any, IN extends any, RES = Target<IN>> (OPTS :{
+				EXP :EXP
+				IN  :IN
+		}) :Test<EXP, RES>
 }
+
+//==============================================================================
+//=== No Arguments ===
+
+;{TRU = Test<EXP>()
+	type EXP =
+		| 'string' | 'number' | 'boolean' | 'symbol'
+		| 'undefined'
+		| 'function' | 'object'
+}
+
+//==============================================================================
+//=== Special Types ===
+
+TRU = Test({} as {
+	IN:  never
+	EXP: never
+})
+
+TRU = Test({} as {
+	IN:  unknown
+	EXP: 'object'
+})
+
+TRU = Test({} as {
+	IN:  any
+	EXP: Target
+})
+
+//==============================================================================
+//=== Value Types - Primitives ===
+
+namespace Case { type EXP = 'string';
+		TRU = Test<EXP, string>()
+		TRU = Test<EXP, 'abc'>()
+}
+
+namespace Case { type EXP = 'number';
+		TRU = Test<EXP, number>()
+		TRU = Test<EXP, 123>()
+}
+
+namespace Case { type EXP = 'boolean';
+		TRU = Test<EXP, boolean>()
+		TRU = Test<EXP, true>()
+		TRU = Test<EXP, false>()
+}
+
+namespace Case { type EXP = 'symbol';
+		TRU = Test<EXP, symbol>()
+}
+
+namespace Case { type EXP = 'undefined';
+		TRU = Test<EXP, undefined>()
+}
+
+//==============================================================================
+//=== Value Types - Non-Primitives ===
+
+namespace Case { type EXP = 'function';
+		TRU = Test<EXP, Function>()
+		TRU = Test<EXP,     () => {}>()
+		TRU = Test<EXP, new () => {}>()
+}
+
+namespace Case { type EXP = 'object';
+		TRU = Test<EXP, object>()
+		TRU = Test<EXP, Object>()
+		TRU = Test<EXP, {}>()
+
+		TRU = Test<EXP, void>()
+		TRU = Test<EXP, null>()
+
+		TRU = Test<EXP, number[]>()
+		TRU = Test<EXP, [1, 2]>()
+
+		TRU = Test<EXP, Set<any>>()
+		TRU = Test<EXP, Map<string, any>>()
+}
+
+//==============================================================================
