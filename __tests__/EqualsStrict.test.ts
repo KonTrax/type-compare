a02f3317b4a40e40a835e41f03acd55f1ed75361
KonTrax <kontraxbea+git@gmail.com>
2018-12-23 22:43:34 +0100

    Even more tests

--- __tests__/EqualsStrict.test.ts
+++ __tests__/EqualsStrict.test.ts
@@ -1,69 +1,279 @@
 import './helpers'
-import {EqualsStrict as Target} from '@ktb/type-compare'
+import {EqualsStrict as _Target} from '@ktb/type-compare'
 
 //==============================================================================
 //=== Setup ===
 
-type  Test   <A, B> =  Target<A, B, TRU, FAL>
-const Test = <A, B> () :[Test<A, B>, Test<B, A>] => [ANY, ANY]
+type Result <AB, BA> = [ AB, BA ]
 
-let TRU :[TRU, TRU]
-let FAL :[FAL, FAL]
+let TRU :Result<TRU, TRU>
+let FAL :Result<FAL, FAL>
+
+type Target <A, B> = _Target<A, B, TRU, FAL>
+
+//==============================================================================
+//=== Helpers ===
+
+const Test = <A, B> () :Test<A, B> => ([ANY, ANY] as any)
+
+type  Test <A, B, AB = Target<A, B>, BA = Target<B, A>> = Result<AB, BA>
+
+const expect :Expect = () => (<any> Test())
+interface Expect {
+	<A> () :{ <B> () :Test<A, B> }
+}
 
 //==============================================================================
 //=== Special Types ===
 
-// CASE: Identical parameters
-TRU = Test<ANY,  ANY>()
-TRU = Test<UNK,  UNK>()
-TRU = Test<NEV,  NEV>()
-TRU = Test<void, void>()
-TRU = Test<VAL,  VAL>()
+	;{const EQ = expect<any>();
+		TRU = EQ<ANY>(); /**/ FAL = EQ<UNK>(); /**/ FAL = EQ<NEV>()
+		FAL = EQ<VAL>(); /**/ FAL = EQ<void>()
+	}
+	;{const EQ = expect<unknown>();
+		FAL = EQ<ANY>(); /**/ TRU = EQ<UNK>(); /**/ FAL = EQ<NEV>()
+		FAL = EQ<VAL>(); /**/ FAL = EQ<void>()
+	}
+	;{const EQ = expect<never>();
+		FAL = EQ<ANY>(); /**/ FAL = EQ<UNK>(); /**/ TRU = EQ<NEV>()
+		FAL = EQ<VAL>(); /**/ FAL = EQ<void>()
+	}
+	;{const EQ = expect<VAL>();
+		FAL = EQ<ANY>(); /**/ FAL = EQ<UNK>(); /**/ FAL = EQ<NEV>()
+		TRU = EQ<VAL>(); /**/ FAL = EQ<void>()
+	}
+	;{const EQ = expect<void>();
+		FAL = EQ<ANY>(); /**/ FAL = EQ<UNK>(); /**/ FAL = EQ<NEV>()
+		FAL = EQ<VAL>(); /**/ TRU = EQ<void>()
+	}
 
-// CASE: any
-FAL = Test<ANY, UNK>() // strict
-FAL = Test<ANY, NEV>()
-FAL = Test<ANY, void>()
-FAL = Test<ANY, VAL>() // strict
+//==============================================================================
+//=== Special Types - Tuples ===
 
-// CASE: unknown
-FAL = Test<UNK, ANY>() // strict
-FAL = Test<UNK, NEV>()
-FAL = Test<UNK, void>()
-FAL = Test<UNK, VAL>()
+	;{const EQ = expect<[any]>();
+		TRU = EQ<[ANY]>(); /**/ FAL = EQ<[UNK]>(); /**/ FAL = EQ<[NEV]>()
+		FAL = EQ<[VAL]>(); /**/ FAL = EQ<[void]>()
+	}
+	;{const EQ = expect<[unknown]>();
+		FAL = EQ<[ANY]>(); /**/ TRU = EQ<[UNK]>(); /**/ FAL = EQ<[NEV]>()
+		FAL = EQ<[VAL]>(); /**/ FAL = EQ<[void]>()
+	}
+	;{const EQ = expect<[never]>();
+		FAL = EQ<[ANY]>(); /**/ FAL = EQ<[UNK]>(); /**/ TRU = EQ<[NEV]>()
+		FAL = EQ<[VAL]>(); /**/ FAL = EQ<[void]>()
+	}
+	;{const EQ = expect<[VAL]>();
+		FAL = EQ<[ANY]>(); /**/ FAL = EQ<[UNK]>(); /**/ FAL = EQ<[NEV]>()
+		TRU = EQ<[VAL]>(); /**/ FAL = EQ<[void]>()
+	}
+	;{const EQ = expect<[void]>();
+		FAL = EQ<[ANY]>(); /**/ FAL = EQ<[UNK]>(); /**/ FAL = EQ<[NEV]>()
+		FAL = EQ<[VAL]>(); /**/ TRU = EQ<[void]>()
+	}
 
-// CASE: never
-FAL = Test<NEV, ANY>()
-FAL = Test<NEV, UNK>()
-FAL = Test<NEV, void>()
-FAL = Test<NEV, VAL>()
+//==============================================================================
+//=== Special Types - Arrays ===
 
-// CASE: void
-FAL = Test<void, ANY>()
-FAL = Test<void, UNK>()
-FAL = Test<void, NEV>()
-FAL = Test<void, VAL>()
+	;{const EQ = expect<any[]>();
+		TRU = EQ<ANY[]>(); /**/ FAL = EQ<UNK[]>(); /**/ FAL = EQ<NEV[]>()
+		FAL = EQ<VAL[]>(); /**/ FAL = EQ<void[]>()
+	}
+	;{const EQ = expect<unknown[]>();
+		FAL = EQ<ANY[]>(); /**/ TRU = EQ<UNK[]>(); /**/ FAL = EQ<NEV[]>()
+		FAL = EQ<VAL[]>(); /**/ FAL = EQ<void[]>()
+	}
+	;{const EQ = expect<never[]>();
+		FAL = EQ<ANY[]>(); /**/ FAL = EQ<UNK[]>(); /**/ TRU = EQ<NEV[]>()
+		FAL = EQ<VAL[]>(); /**/ FAL = EQ<void[]>()
+	}
+	;{const EQ = expect<VAL[]>();
+		FAL = EQ<ANY[]>(); /**/ FAL = EQ<UNK[]>(); /**/ FAL = EQ<NEV[]>()
+		TRU = EQ<VAL[]>(); /**/ FAL = EQ<void[]>()
+	}
+	;{const EQ = expect<void[]>();
+		FAL = EQ<ANY[]>(); /**/ FAL = EQ<UNK[]>(); /**/ FAL = EQ<NEV[]>()
+		FAL = EQ<VAL[]>(); /**/ TRU = EQ<void[]>()
+	}
 
-// CASE: VALUE
-FAL = Test<VAL, UNK>()
-FAL = Test<VAL, ANY>() // strict
-FAL = Test<VAL, NEV>()
-FAL = Test<VAL, void>()
+//==============================================================================
+//=== Tuples ===
+
+	// CASE: Must have equal length
+
+	FAL = Test<[any], []>()
+	TRU = Test<[any], [any]>()
+	FAL = Test<[any], [any, any]>()
+
+	// CASE: Correctly handles optional fields - Valid
+
+	TRU = Test<[number, number?], [number, (number)?]>()
+	TRU = Test<[number, number?], [number, (number | undefined)?]>()
+
+	// CASE: Correctly handles optional fields - Invalid
+
+	FAL = Test<[number, number?], [number, number]>()
+	FAL = Test<[number, number?], [number, number | undefined | null]>()
+	FAL = Test<[number, number?], [number, number | undefined]>()
+	FAL = Test<[number, number?], [number, number | never]>()
+	FAL = Test<[number, number?], [number, number | null]>()
+
+	// CASE: Not equal to Array
+
+	FAL = Test<any[], []>()
+	FAL = Test<any[], [any, any]>()
+
+	FAL = Test<[any], any[]>()
+
+	// CASE: Should not match array
+
+	;{type TYP = any;
+		FAL = Test<TYP[], [ANY]>(); /**/ FAL = Test<TYP[], [UNK]>()
+		FAL = Test<TYP[], [NEV]>(); /**/ FAL = Test<TYP[], [VAL]>()
+	}
+
+	;{type TYP = unknown;
+		FAL = Test<TYP[], [ANY]>(); /**/ FAL = Test<TYP[], [UNK]>()
+		FAL = Test<TYP[], [NEV]>(); /**/ FAL = Test<TYP[], [VAL]>()
+	}
+
+	;{type TYP = never;
+		FAL = Test<TYP[], [ANY]>(); /**/ FAL = Test<TYP[], [UNK]>()
+		FAL = Test<TYP[], [NEV]>(); /**/ FAL = Test<TYP[], [VAL]>()
+	}
+
+	// CASE: Extra Special Type tests
+
+	;{type TYP = any;
+		TRU = Test<[TYP], [ANY]>(); /**/ TRU = Test<[TYP, TYP], [ANY, ANY]>()
+		FAL = Test<[TYP], [UNK]>(); /**/ FAL = Test<[TYP, TYP], [UNK, UNK]>()
+		FAL = Test<[TYP], [NEV]>(); /**/ FAL = Test<[TYP, TYP], [NEV, NEV]>()
+		FAL = Test<[TYP], [VAL]>(); /**/ FAL = Test<[TYP, TYP], [VAL, VAL]>()
+	}
+
+	;{type TYP = unknown;
+		FAL = Test<[TYP], [ANY]>(); /**/ FAL = Test<[TYP, TYP], [ANY, ANY]>()
+		TRU = Test<[TYP], [UNK]>(); /**/ TRU = Test<[TYP, TYP], [UNK, UNK]>()
+		FAL = Test<[TYP], [NEV]>(); /**/ FAL = Test<[TYP, TYP], [NEV, NEV]>()
+		FAL = Test<[TYP], [VAL]>(); /**/ FAL = Test<[TYP, TYP], [VAL, VAL]>()
+	}
+
+	;{type TYP = never;
+		FAL = Test<[TYP], [ANY]>(); /**/ FAL = Test<[TYP, TYP], [ANY, ANY]>()
+		FAL = Test<[TYP], [UNK]>(); /**/ FAL = Test<[TYP, TYP], [UNK, UNK]>()
+		TRU = Test<[TYP], [NEV]>(); /**/ TRU = Test<[TYP, TYP], [NEV, NEV]>()
+		FAL = Test<[TYP], [VAL]>(); /**/ FAL = Test<[TYP, TYP], [VAL, VAL]>()
+	}
+
+	;{type TYP = VAL;
+		FAL = Test<[TYP], [ANY]>(); /**/ FAL = Test<[TYP, TYP], [ANY, ANY]>()
+		FAL = Test<[TYP], [UNK]>(); /**/ FAL = Test<[TYP, TYP], [UNK, UNK]>()
+		FAL = Test<[TYP], [NEV]>(); /**/ FAL = Test<[TYP, TYP], [NEV, NEV]>()
+		TRU = Test<[TYP], [VAL]>(); /**/ TRU = Test<[TYP, TYP], [VAL, VAL]>()
+	}
+
+//==============================================================================
+//=== Arrays ===
+
+	// CASE: Works correctly with all variations
+
+	TRU = Test<any[], any[]>()
+	TRU = Test<any[], Array<any>>()
+	TRU = Test<any[], [...any[]]>()
 
 //==============================================================================
 //=== Objects ===
 
-// CASE:
-FAL = Test<{ prop :true }, {}>()
+	// CASE:
 
-// CASE: Empty object should not equal all optional props object
-FAL = Test<{ prop ?:true }, {}>()
+	FAL = Test<{ prop :true }, {}>()
 
-// CASE: Optional object prop should equal itself
-TRU = Test<{ prop ?:true }, { prop ?:true }>()
-TRU = Test<{ prop ?:true }, { prop ?:true | undefined }>()
+	// CASE: Empty object should not equal all optional props object
 
-// CASE: Optional object prop should not equal required | undefined
-FAL = Test<{ prop ?:true }, { prop :true | undefined }>()
+	FAL = Test<{ prop ?:true }, {}>()
+
+	// CASE: Optional object prop should equal itself
+
+	TRU = Test<{ prop ?:true }, { prop ?:true }>()
+	TRU = Test<{ prop ?:true }, { prop ?:true | undefined }>()
+
+	// CASE: Optional object prop should not equal required | undefined
+
+	FAL = Test<{ prop ?:true }, { prop :true | undefined }>()
+
+	// CASE: Matching object and intersection is equal
+
+	TRU = Test<{ x :1 } & { y :2 }, { x :1, y :2 }>()
+
+	// CASE: Matching object and intersection is not equal if any prop types diff
+
+	FAL = Test<{ x :1 } & { y :2 }, { x :1, y :'conflict' }>()
 
 //==============================================================================
+;{//=== Functions ===
+
+	//=== FIXTURES ===
+
+	type FN_A  = ()          => string
+	type FN_B  = (x :string) => number
+	type FN_AB = {
+			()          :string
+			(x :string) :number
+	};
+	type FN_BA = {
+			(x :string) :number
+			()          :string
+	};
+	type FN_PROPS = {
+			kind :'test'
+	}
+	type FN_W_PROPS = {
+			()   :string
+			kind :'test'
+	};
+
+	// Function - Props: is equal to self
+	TRU = Test<FN_W_PROPS, FN_W_PROPS>()
+
+	// Function - Props: Not equal to partial
+	FAL = Test<FN_W_PROPS, FN_A>()
+	FAL = Test<FN_W_PROPS, FN_PROPS>()
+
+	/// TODO: Currently not possible
+	// Function - Props: Match callable type with intersection
+	// TRU = Test<FN_W_PROPS, FN_A & FN_PROPS>()
+	// TRU = Test<FN_W_PROPS, FN_PROPS & FN_A>()
+
+	/// NOTE: Function overloads and intersections are order-dependent
+
+	// Function - Overload: is equal to self
+	TRU = Test<FN_AB, FN_AB>()
+	TRU = Test<FN_BA, FN_BA>()
+
+	// Function - Overload: order has to be the same
+	FAL = Test<FN_AB, FN_BA>()
+	FAL = Test<FN_BA, FN_AB>()
+
+	// Function - Overload: Not equal to matching intersection type
+	FAL = Test<FN_AB, FN_B & FN_A>()
+	FAL = Test<FN_BA, FN_A & FN_B>()
+
+	// Function - Overload: Not equal to matching intersection type in the same order
+	FAL = Test<FN_AB, FN_A & FN_B>()
+	FAL = Test<FN_BA, FN_B & FN_A>()
+
+	// Function - Intersection: TRU - same signatures - same order
+	TRU = Test<FN_A & FN_B, FN_A & FN_B>()
+	TRU = Test<FN_B & FN_A, FN_B & FN_A>()
+
+	// Function - Intersection: TRU - same signatures - diff order
+	TRU = Test<FN_A & FN_B, FN_B & FN_A>()
+	TRU = Test<FN_B & FN_A, FN_A & FN_B>()
+
+	// Function - Intersection: FAL - vs partial
+	FAL = Test<FN_A & FN_B, FN_A>()
+	FAL = Test<FN_A & FN_B, FN_B>()
+
+	FAL = Test<FN_B & FN_A, FN_A>()
+	FAL = Test<FN_B & FN_A, FN_B>()
+
+}//=============================================================================
