cb16c0a0abceaa03b2f44a23f0c1d0b54dd3bfa6
KonTrax <kontraxbea+git@gmail.com>
2018-12-23 22:34:47 +0100

    Improved equality comparison

--- src/EqualsStrict.ts
+++ src/EqualsStrict.ts
@@ -1,5 +1,12 @@
 import { EqualsUnknownOrAny } from './EqualsUnknownOrAny'
 
+export {
+	// EqualsStrict1 as EqualsStrict
+	// EqualsStrict2 as EqualsStrict
+	EqualsStrict3 as EqualsStrict
+	// EqualsStrict4 as EqualsStrict
+}
+
 /**
  * Strict type equality comparison
  *
@@ -12,7 +19,7 @@ import { EqualsUnknownOrAny } from './EqualsUnknownOrAny'
  * - `any`     only equals `any`
  * - `unknown` only equals `unknown`
  */
-export type EqualsStrict <A, B, THEN = A, ELSE = never> =
+export type EqualsStrict1 <A, B, THEN = A, ELSE = never> =
 (
 		| // (A || B) === UNK | ANY
 		[unknown] extends [A | B] ? EqualsUnknownOrAny<A, B, THEN, ELSE>
@@ -25,4 +32,45 @@ export type EqualsStrict <A, B, THEN = A, ELSE = never> =
 
 		: // (A != B) === ?
 		ELSE
+)
+
+/**
+ * Strict type equality comparison
+ *
+ * Wraps `A` & `B` in [] for comparison
+ *
+ * Useful for writing tests for types
+ *
+ * **Features**:
+ * - `never`   only equals `never`
+ * - `any`     only equals `any`
+ * - `unknown` only equals `unknown`
+ */
+export type EqualsStrict2 <A, B, THEN = A, ELSE = never> =
+(
+	(<G> () => G extends A ? 1 : 2) extends (<G> () => G extends B ? 1 : 2)
+			? THEN : ELSE
+)
+
+/**
+ * Strict type equality comparison
+ *
+ * Wraps `A` & `B` in [] for comparison
+ *
+ * Useful for writing tests for types
+ *
+ * **Features**:
+ * - `never`   only equals `never`
+ * - `any`     only equals `any`
+ * - `unknown` only equals `unknown`
+ */
+export type EqualsStrict3 <A, B, THEN = A, ELSE = never> =
+(
+		(EqualsStrict2<A, B, 1, 0>) extends (1)
+			? [A, B] extends [B, A] ? THEN : ELSE
+			: (
+				[A] extends [any[] | Function] ? ELSE :
+				[B] extends [any[] | Function] ? ELSE :
+				EqualsStrict1<A, B, THEN, ELSE>
+		)
 )
