917f632c66f2e0a18b5890f1816dfd539a363ea1
KonTrax <kontraxbea+git@gmail.com>
2019-06-21 06:08:17 +0200

    more tests

--- __tests__/is/IsSpecial.test.ts
+++ __tests__/is/IsSpecial.test.ts
@@ -7,9 +7,90 @@ import {IsSpecial as Target} from '@ktb/type-compare'
 const Test = <T> () :Target<T, TRU, FAL> => ANY
 
 //==============================================================================
+//=== Tests ===
 
-TRU = Test<any>()
-TRU = Test<unknown>()
-TRU = Test<never>()
-FAL = Test<void>()
-FAL = Test<VAL>()
+//==============================================================================
+//=== Special Types ===
+
+	TRU = Test<never>()
+
+	TRU = Test<any>()
+	TRU = Test<unknown>()
+
+//==============================================================================
+//=== Special Value Types ===
+
+	FAL = Test<{}>()
+
+	FAL = Test<void>()
+
+	FAL = Test<VAL>()
+
+//==============================================================================
+//=== Unsorted ===
+
+	//=== Non-values ===
+
+	FAL = Test<undefined>()
+	FAL = Test<null>()
+
+	//=== Numbers ===
+
+	FAL = Test<number>()
+	FAL = Test<Number>()
+
+	FAL = Test< 0>()
+	FAL = Test<-0>()
+	FAL = Test< 1>()
+	FAL = Test<-1>()
+
+	FAL = Test<bigint>()
+
+	//=== Strings ===
+
+	FAL = Test<string>()
+	FAL = Test<String>()
+
+	FAL = Test<''>()
+	FAL = Test<'abc'>()
+
+	//=== Booleans ===
+
+	FAL = Test<boolean>()
+
+	//=== Functions ===
+
+	FAL = Test<Function>()
+
+	//=== Arrays ===
+
+	FAL = Test<Array<any>>()
+	FAL = Test<any[]>()
+
+	//=== Tuples ===
+
+	FAL = Test<[]>()
+
+	FAL = Test<[any ]>()
+	FAL = Test<[any?]>()
+
+	//=== Base objects ===
+
+	FAL = Test<object>()
+	FAL = Test<Object>()
+
+	//=== Simple objects ===
+
+	FAL = Test<{ a :1 }>()
+
+	//=== Indexed objects ===
+
+	FAL = Test<{ [x :string] :any }>()
+	FAL = Test<{ [x :number] :any }>()
+	FAL = Test<{ [x :string] :any, [x :number] :any }>()
+
+	//=== Advanced objects ===
+
+	// FAL = Test<{}>()
+
+//==============================================================================
--- __tests__/is/IsValue.test.ts
+++ __tests__/is/IsValue.test.ts
@@ -7,9 +7,90 @@ import {IsValue as Target} from '@ktb/type-compare'
 const Test = <T> () :Target<T, TRU, FAL> => ANY
 
 //==============================================================================
+//=== Tests ===
 
-FAL = Test<any>()
-FAL = Test<unknown>()
-FAL = Test<never>()
-TRU = Test<void>()
-TRU = Test<VAL>()
+//==============================================================================
+//=== Special Types ===
+
+	FAL = Test<never>()
+
+	FAL = Test<any>()
+	FAL = Test<unknown>()
+
+//==============================================================================
+//=== Special Value Types ===
+
+	TRU = Test<{}>()
+
+	TRU = Test<void>()
+
+	TRU = Test<VAL>()
+
+//==============================================================================
+//=== Unsorted ===
+
+	//=== Non-values ===
+
+	TRU = Test<undefined>()
+	TRU = Test<null>()
+
+	//=== Numbers ===
+
+	TRU = Test<number>()
+	TRU = Test<Number>()
+
+	TRU = Test< 0>()
+	TRU = Test<-0>()
+	TRU = Test< 1>()
+	TRU = Test<-1>()
+
+	TRU = Test<bigint>()
+
+	//=== Strings ===
+
+	TRU = Test<string>()
+	TRU = Test<String>()
+
+	TRU = Test<''>()
+	TRU = Test<'abc'>()
+
+	//=== Booleans ===
+
+	TRU = Test<boolean>()
+
+	//=== Functions ===
+
+	TRU = Test<Function>()
+
+	//=== Arrays ===
+
+	TRU = Test<Array<any>>()
+	TRU = Test<any[]>()
+
+	//=== Tuples ===
+
+	TRU = Test<[]>()
+
+	TRU = Test<[any ]>()
+	TRU = Test<[any?]>()
+
+	//=== Base objects ===
+
+	TRU = Test<object>()
+	TRU = Test<Object>()
+
+	//=== Simple objects ===
+
+	TRU = Test<{ a :1 }>()
+
+	//=== Indexed objects ===
+
+	TRU = Test<{ [x :string] :any }>()
+	TRU = Test<{ [x :number] :any }>()
+	TRU = Test<{ [x :string] :any, [x :number] :any }>()
+
+	//=== Advanced objects ===
+
+	// TRU = Test<{}>()
+
+//==============================================================================
