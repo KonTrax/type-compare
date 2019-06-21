8343fcd35e11f5c91e1f01f9369ccc1ffd257533
KonTrax <kontraxbea+git@gmail.com>
2019-06-21 06:06:45 +0200

    added bigint support + additional tests

--- __tests__/TypeName.test.ts
+++ __tests__/TypeName.test.ts
@@ -16,6 +16,8 @@ const Test = (() => ANY) as {
 				EXP :EXP
 				IN  :IN
 		}) :Test<EXP, RES>
+
+		<EXP, IN, RES = Target<IN>> (exp :EXP, res :IN) :Test<EXP, RES>
 }
 
 //==============================================================================
@@ -23,9 +25,13 @@ const Test = (() => ANY) as {
 
 ;{TRU = Test<EXP>()
 	type EXP =
-		| 'string' | 'number' | 'boolean' | 'symbol'
+		| 'string'
+		| 'number' | 'bigint'
+		| 'boolean'
+		| 'symbol'
 		| 'undefined'
-		| 'function' | 'object'
+		| 'function'
+		| 'object'
 }
 
 //==============================================================================
@@ -38,31 +44,90 @@ TRU = Test({} as {
 
 TRU = Test({} as {
 	IN:  unknown
-	EXP: 'object'
+	EXP: unknown
 })
 
 TRU = Test({} as {
 	IN:  any
-	EXP: Target
+	EXP:
+		| 'string'
+		| 'number' | 'bigint'
+		| 'boolean'
+		| 'symbol'
+		| 'undefined'
+
+		| 'function'
+		| 'object'
 })
 
 //==============================================================================
 //=== Value Types - Primitives ===
 
-namespace Case { type EXP = 'string';
+namespace Case { type EXP = 'string'; const EXP :EXP = 'string'
 		TRU = Test<EXP, string>()
+
 		TRU = Test<EXP, 'abc'>()
+		TRU = Test<EXP, ''>()
+
+		TRU = Test(EXP, String(''))
+
+		//=== Enums - String
+
+		enum StringEnum { A = 'A', B = 'B' }
+		TRU = Test(EXP, StringEnum.A)
 }
 
-namespace Case { type EXP = 'number';
+namespace Case { type EXP = 'number'; const EXP :EXP = 'number'
 		TRU = Test<EXP, number>()
-		TRU = Test<EXP, 123>()
+
+		TRU = Test<EXP,  123>()
+		TRU = Test<EXP,    0>()
+		TRU = Test<EXP,   -0>()
+
+		TRU = Test(EXP, Number(0))
+		TRU = Test(EXP, Number('0'))
+
+		//=== Notations ===
+
+		TRU = Test<EXP, 0b0010 /*   2 */>() // Binary
+		TRU = Test<EXP, 0o020  /*  16 */>() // Octal
+		TRU = Test<EXP, 0x100  /* 256 */>() // Hexadecimal
+
+		//=== Enums ===
+
+		// Standard
+		enum NumberEnum { A, B }
+		TRU = Test(EXP, NumberEnum.A)
+
+		// Binary
+		enum BinaryEnum { A = 0b1, B = 0b10 }
+		TRU = Test(EXP, BinaryEnum.A)
 }
 
-namespace Case { type EXP = 'boolean';
+namespace Case { type EXP = 'bigint'; const EXP :EXP = 'bigint'
+		TRU = Test<EXP, bigint>()
+
+		TRU = Test<EXP,  123n>()
+		TRU = Test<EXP,    0n>()
+		TRU = Test<EXP,   -0n>()
+
+		TRU = Test(EXP, BigInt(0))
+		TRU = Test(EXP, BigInt('0'))
+
+		//=== Notations ===
+
+		TRU = Test<EXP, 0b0010n /*   2n */>() // Binary
+		TRU = Test<EXP, 0o020n  /*  16n */>() // Octal
+		TRU = Test<EXP, 0x100n  /* 256n */>() // Hexadecimal
+}
+
+namespace Case { type EXP = 'boolean'; const EXP :EXP = 'boolean'
 		TRU = Test<EXP, boolean>()
+
 		TRU = Test<EXP, true>()
 		TRU = Test<EXP, false>()
+
+		TRU = Test(EXP, Boolean(''))
 }
 
 namespace Case { type EXP = 'symbol';
@@ -77,24 +142,61 @@ namespace Case { type EXP = 'undefined';
 //=== Value Types - Non-Primitives ===
 
 namespace Case { type EXP = 'function';
+
+		//=== Base
+
 		TRU = Test<EXP, Function>()
-		TRU = Test<EXP,     () => {}>()
-		TRU = Test<EXP, new () => {}>()
+
+		//=== Standard-Functions
+
+		TRU = Test<EXP, {     () :void }>()
+		TRU = Test<EXP, { new () :String }>()
+
+		//=== Arrow-Functions
+
+		TRU = Test<EXP,     () => void>()
+		TRU = Test<EXP, new () => String>()
 }
 
 namespace Case { type EXP = 'object';
+
+		//=== Base objects
+
 		TRU = Test<EXP, object>()
 		TRU = Test<EXP, Object>()
 		TRU = Test<EXP, {}>()
 
-		TRU = Test<EXP, void>()
+		//=== Null object
+
 		TRU = Test<EXP, null>()
 
+		//=== Arrays
+
 		TRU = Test<EXP, number[]>()
+
+		//=== Tuples
+
+		TRU = Test<EXP, []>()
 		TRU = Test<EXP, [1, 2]>()
+
+		//=== Collections
 
 		TRU = Test<EXP, Set<any>>()
 		TRU = Test<EXP, Map<string, any>>()
+
+		//=== Other
+
+		TRU = Test<EXP, Promise<any>>()
+}
+
+//==============================================================================
+//=== Value Types - Fallback & Other ===
+
+namespace Case { type EXP = unknown;
+
+		//=== Void
+
+		TRU = Test<EXP, void>()
 }
 
 //==============================================================================
--- src/TypeName.ts
+++ src/TypeName.ts
@@ -1,13 +1,21 @@
+import { IsAny } from './is/IsAny'
+
 /**
- * Get string literal name of type `T`
+ * Get string literal name of type `T` used by javascript runtime
+ *
+ * - `void` is considered as `unknown`
  */
 export type TypeName <T = any> =
 		| T extends string    ? 'string'
 		: T extends number    ? 'number'
+		: T extends bigint    ? 'bigint'
 		: T extends boolean   ? 'boolean'
 		: T extends symbol    ? 'symbol'
 
 		: T extends undefined ? 'undefined'
 
 		: T extends Function  ? 'function'
-		:                       'object'
+		: T extends null      ? 'object'
+		: T extends object    ? 'object'
+
+		: IsAny<T, never, unknown>
